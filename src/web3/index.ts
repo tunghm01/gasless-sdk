24a6598cae91c2161b1cc09ca76de89b50f6d105
tunghm01 <tunghoang@remitano.com>
2023-03-15 09:35:30 +0700

    rm useless functions

--- src/web3/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./address-util";
-export * from "./ata-util";
-export * from "./public-key-utils";
-export * from "./token-util";
-export * from "./transactions";
--- src/web3/transactions/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./transactions-builder";
-export * from "./transactions-processor";
-export * from "./types";
